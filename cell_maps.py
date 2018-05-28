840ab5aee2f74d7d09780b46dfcbebb752e3b890
Simone <simone.procaccia@gmail.com>
2018-05-28 20:44:58 +0200

    updating hyperparams for epochs

--- cell_maps.py
+++ cell_maps.py
@@ -20,7 +20,7 @@ num_hidden_units = [1024, 512, 256, 128]
 # model = tf.estimator.DNNClassifier(feature_columns=feature_columns,
 #                                    n_classes=10, model_dir="./checkpoints_FFN/", hidden_units=num_hidden_units)
 
-model = tf.estimator.Estimator(model_fn=mo.cnn, params={'learning_rate': 1e-3, 'n_classes': 10, 'pooling': True},
+model = tf.estimator.Estimator(model_fn=mo.cnn, params={'learning_rate': 1e-4, 'n_classes': 10, 'pooling': True},
                                model_dir='./checkpoints_prova/')
 
 print("Model built")
@@ -33,7 +33,7 @@ for i in range(1, 5):
 
     train_input_fn = tf.estimator.inputs.numpy_input_fn(x={'X': x_part},
                                                         y=y_part,
-                                                        num_epochs=1,
+                                                        num_epochs=1000,
                                                         batch_size=128,
                                                         shuffle=False)
 
